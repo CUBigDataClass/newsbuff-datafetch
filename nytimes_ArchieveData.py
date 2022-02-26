dbdb8e7d50bc5f9a30cbefd2c092ef7b6f3d78f3
anushab97 <78228113+anushab97@users.noreply.github.com>
2022-02-26 16:40:35 -0700

    Python code to get archive data from nytimes Archive API
    Also a sample output file having news data from a month for 2018-2-1 to 2018-2-28

---
+++ nytimes_ArchieveData.py
@@ -0,0 +1,40 @@
+"nytimes"
+
+from asyncio.windows_events import NULL
+from pynytimes import NYTAPI
+import datetime
+
+myKey = "br2UmSMINrYwhsA6D3B1zzkJ2pLQ6eVV"
+#outputFile = "C:\Users\Shiva\Desktop\Anu\UC BOULDERS\Courses\Spring 2022\ATLS 5214_Big Data Architecture\Project\output.txt"
+nyt = NYTAPI(myKey, parse_dates=True)
+
+class Article:
+    def __init__(self, dateTime, section, subsection, headline, description, location, webURL, imageURL):
+        self.dateTime = dateTime
+        self.section = section
+        self.subsection = subsection
+        self.headline = headline
+        self.description = description
+        self.locatuon = location
+        self.webURL = webURL
+        self.imageURL = imageURL
+
+
+def getData(year, month):
+    data = nyt.archive_metadata(
+    date = datetime.datetime(year, month, 1))
+    nyt.close()
+    return data
+
+
+
+
+
+
+print("hello")
+#print(len(data))
+#file1 = open(r"C:\Users\Shiva\Desktop\Anu\UC BOULDERS\Courses\Spring 2022\ATLS 5214_Big Data Architecture\Project\output.txt","w+", encoding="utf-8")
+#file1.write(str(data))
+
+#print (data)
+#file1.close()
