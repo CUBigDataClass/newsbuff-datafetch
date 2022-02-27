9e0d1b077fd96d6d3abb608eac3896f4176b5b0b
anusha97 <anusha.basavaraja@colorado.edu>
2022-02-26 17:22:58 -0700

    adding some modifications.

--- nytimes_ArchieveData.py
+++ nytimes_ArchieveData.py
@@ -4,10 +4,11 @@ from asyncio.windows_events import NULL
 from pynytimes import NYTAPI
 import datetime
 
-myKey = "xxx"
+myKey = ""
 #outputFile = "C:\Users\Shiva\Desktop\Anu\UC BOULDERS\Courses\Spring 2022\ATLS 5214_Big Data Architecture\Project\output.txt"
 nyt = NYTAPI(myKey, parse_dates=True)
 
+"""
 class Article:
     def __init__(self, dateTime, section, subsection, headline, description, location, webURL, imageURL):
         self.dateTime = dateTime
@@ -18,6 +19,7 @@ class Article:
         self.locatuon = location
         self.webURL = webURL
         self.imageURL = imageURL
+"""
 
 
 def getData(year, month):
@@ -27,6 +29,20 @@ def getData(year, month):
     return data
 
 
+myArticle = []
+for year in range(1850, 2022, -1):
+    myArticle.clear()
+    for month in range(1, 13, 1):
+        article = getData(year, month)
+        for a in article:
+            if "subsection_name" in a.keys():
+                ssname = a['subsection_name']
+            else:
+                ssname = "Null"
+            elements = {datetime:a["pub_date"] , "section":a['section_name'] , "subsection":ssname , "headline":a['abstract'] , 
+            "description":a['lead_paragraph'] , "location": , "webURL":a['web_url'] , "imageURL": }
+            myArticle.append(str(elements))
+            
 
 
 
