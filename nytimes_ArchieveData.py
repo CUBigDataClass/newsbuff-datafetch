752d49f54b12139d3ce21776f284d2fe433a4900
saba6167 <89482157+saba6167@users.noreply.github.com>
2022-02-26 19:24:29 -0700

    Added location

--- nytimes_ArchieveData.py
+++ nytimes_ArchieveData.py
@@ -21,7 +21,6 @@ class Article:
         self.imageURL = imageURL
 """
 
-
 def getData(year, month):
     data = nyt.archive_metadata(
     date = datetime.datetime(year, month, 1))
@@ -33,21 +32,28 @@ myArticle = []
 for year in range(1850, 2022, -1):
     myArticle.clear()
     for month in range(1, 13, 1):
-        article = getData(year, month)
-        for a in article:
-            if "subsection_name" in a.keys():
-                ssname = a['subsection_name']
+        articles = getData(year, month)
+        for article in articles:
+            if "subsection_name" in article.keys():
+                subsection_name = article['subsection_name']
             else:
-                ssname = "Null"
-            elements = {datetime:a["pub_date"] , "section":a['section_name'] , "subsection":ssname , "headline":a['abstract'] , 
-            "description":a['lead_paragraph'] , "location": , "webURL":a['web_url'] , "imageURL": }
-            myArticle.append(str(elements))
-            
+                subsection_name = "null"
 
+            if "multimedia" in article.keys() and len(article['multimedia']) > 0:
+                    imageURL = article['multimedia'][0]['url']
+            else:
+                imageURL = "null"
 
+            for keyword in article["keywords"]:
+                if keyword["name"] == "glocations":
+                    location = keyword["value"]
+                    elements = {"datetime":article["pub_date"] , "section":article['section_name'] , "subsection":subsection_name , 
+                    "headline":article['abstract'] , "description":article['lead_paragraph'] , "location":location , 
+                    "webURL":article['web_url'] , "imageURL":imageURL}
+                    myArticle.append(str(elements))
 
-
-print("hello")
+print(myArticle[0])
+          
 #print(len(data))
 #file1 = open(r"C:\Users\Shiva\Desktop\Anu\UC BOULDERS\Courses\Spring 2022\ATLS 5214_Big Data Architecture\Project\output.txt","w+", encoding="utf-8")
 #file1.write(str(data))
