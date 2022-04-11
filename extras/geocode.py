ff319bb837f1cd1c22f32bb048383df10cde89a9
sspathare97 <sspathare97@gmail.com>
2022-04-11 00:00:20 -0600

    bing limit

--- extras/geocode.py
+++ extras/geocode.py
@@ -6,12 +6,14 @@ sys.path.insert(0, parentdir)
 import env
 
 # locations = ['PHILIPSE MANOR HALL (YONKERS, NY)', 'BALDWIN', 'GREAT NECK ESTATES, NY', 'Europe', 'North Pole']
-locations = json.load(open('locations1.json', 'r'))
+locations = json.load(open('locations.json', 'r'))
 # print(locations)
+
+locations = locations[:50]
 
 g = geocoder.bing(locations, method='batch', key=env.bing_api_key)
 res = []
 for result in g:
    res.append(result.latlng)
 # print(res)
-json.dump(res, open('location1res.json', 'w'), indent=2)
+json.dump(res, open('locationres.json', 'w'), indent=2)
