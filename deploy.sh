07135540b2f253495f770608c42295f2ceb34900
saumya-bansal <saumzz19@gmail.com>
2022-04-17 22:59:52 -0600

    github workflow

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp example.app.yaml app.yaml
-sed -i 's/DB_URL: ""/DB_URL: ""/' app.yaml
-sed -i 's/NYT_API_KEY: ""/NYT_API_KEY: ""/' app.yaml
-sed -i 's/GOOGLE_API_KEY: ""/GOOGLE_API_KEY: ""/' app.yaml
+sed -i 's/DB_URL: ""/DB_URL: "${{ secrets.GCP_PROJECT }}"/' app.yaml
+sed -i 's/NYT_API_KEY: ""/NYT_API_KEY: "${{ secrets.NYT_API_KEY }}"/' app.yaml
+sed -i 's/GOOGLE_API_KEY: ""/GOOGLE_API_KEY: "${{ secrets.GOOGLE_API_KEY }}"/' app.yaml
+
