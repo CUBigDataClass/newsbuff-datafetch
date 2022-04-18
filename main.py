a2f639e098df95a9f2c36fa109f4693e5457fb99
sspathare97 <sspathare97@gmail.com>
2022-04-18 15:36:16 -0600

    added swagger

--- main.py
+++ main.py
@@ -5,10 +5,18 @@ import pymongo
 from bson import ObjectId
 from flask import Flask, Response, request
 from flask_cors import CORS
+from flask_restx import Api, Resource
 import mongodb
 
 app = Flask(__name__)
 CORS(app)
+
+api = Api(app, 
+		  version = "1.0", 
+		  title = "News Buff Backend", 
+		  description = "REST APIs for fetching news articles")
+
+name_space = api.namespace('api', description='Main APIs')
 
 client = mongodb.dbConnection()
 mydb = mongodb.createdb(client)
@@ -22,43 +30,49 @@ class MongoDbEncoder(json.JSONEncoder):
             return str(obj)
         return json.JSONEncoder.default(self, obj)
 
-@app.get("/api/ping")
-def get_ping():
-    return Response(
-        json.dumps({ "success": True }),
-        mimetype='application/json'
-    )
-
-@app.get("/api/<year>/<month>/<day>")
-def get_articles(year, month, day):
-    params = request.args
-    try:
-        startDate = datetime(int(year), int(month), int(day))
-        endDate = startDate + timedelta(days=1)
-    except:
+@name_space.route("/ping")
+class Class1(Resource):
+    @api.doc(responses={ 200: 'OK' })
+    def get(self):
         return Response(
-            json.dumps({ "success": False, "count": 0, "rows": [] }),
+            json.dumps({ "success": True }),
             mimetype='application/json'
         )
 
-    try:
-        query = { "dateTime": { "$gte": startDate, "$lt": endDate } }
-        if 'polygon' in params:
-            polygon = json.loads(params['polygon'])
-            # polygon = [ [ [ 0 , 0 ] , [ 3 , 6 ] , [ 6 , 1 ] , [ 0 , 0  ] ] ]
-            query['locations'] = { '$elemMatch': {'location': { '$geoWithin': { '$geometry': { 'type': 'Polygon', 'coordinates': polygon } } } } }
-        results = articleCollection.find(query, {'_id': False}).sort('dateTime', pymongo.DESCENDING).limit(100)
-        resultsList = list(results)
-        resultsCount = len(resultsList)
-        return Response(
-            json.dumps({ "success": True, "count": resultsCount, "rows": resultsList }, cls=MongoDbEncoder),
-            mimetype='application/json'
-        )
-    except:
-        return Response(
-            json.dumps({ "success": False, "count": 0, "rows": [] }),
-            mimetype='application/json'
-        )
+@name_space.route("/<int:year>/<int:month>/<int:day>")
+class Class2(Resource):
+    @api.doc(responses={ 200: 'OK', 400: 'Invalid parameters', 500: 'Something went wrong' }, 
+            params={ 'year': 'Year', 'month': 'Month', 'day': 'Day',
+                        'polygon': {'in': 'query', 'description': 'GeoJSON Polygon with a Single Ring'} })
+    def get(self, year, month, day):
+        params = request.args
+        try:
+            startDate = datetime(int(year), int(month), int(day))
+            endDate = startDate + timedelta(days=1)
+        except:
+            return Response(
+                json.dumps({ "success": False, "count": 0, "rows": [] }),
+                mimetype='application/json'
+            )
+
+        try:
+            query = { "dateTime": { "$gte": startDate, "$lt": endDate } }
+            if 'polygon' in params:
+                polygon = json.loads(params['polygon'])
+                # polygon = [ [ [ 0 , 0 ] , [ 3 , 6 ] , [ 6 , 1 ] , [ 0 , 0  ] ] ]
+                query['locations'] = { '$elemMatch': {'location': { '$geoWithin': { '$geometry': { 'type': 'Polygon', 'coordinates': polygon } } } } }
+            results = articleCollection.find(query, {'_id': False}).sort('dateTime', pymongo.DESCENDING).limit(100)
+            resultsList = list(results)
+            resultsCount = len(resultsList)
+            return Response(
+                json.dumps({ "success": True, "count": resultsCount, "rows": resultsList }, cls=MongoDbEncoder),
+                mimetype='application/json'
+            )
+        except:
+            return Response(
+                json.dumps({ "success": False, "count": 0, "rows": [] }),
+                mimetype='application/json'
+            )
 
 if __name__ == '__main__':
     app.run(debug=True)
